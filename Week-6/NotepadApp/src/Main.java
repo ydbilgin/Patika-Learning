04fe0180f6f369acfc337f1c3b3de77115fac4f7
ydbilgin <yasinderyabilgin@gmail.com>
2023-10-18 05:00:43 +0300

    Notepad

---
+++ Week-6/NotepadApp/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir metin girin: ");
+        String metin = input.nextLine();
+        String file = "src/notlar.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(file,true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println(metin);
+            printWriter.close();
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String okunanMetin = bufferedReader.readLine();
+
+            while (okunanMetin != null) {
+                System.out.println(okunanMetin);
+                okunanMetin = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+    }
+}
